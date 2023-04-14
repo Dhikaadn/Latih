148a7c3d3b60284917ea3158bddfe86db5eedb6f
Dhikaadn <andhikadianpratama057@gmail.com>
2023-04-14 14:06:41 +0700

    first commit

---
+++ src/edit-table/EditTable.js
@@ -0,0 +1,60 @@
+import "./EditTable.css";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+const EditTable = () => {
+  const [title, setTitle] = useState("");
+  //   const [price, setPrice] = useState("");
+  const { id } = useParams();
+  //   const navigate = useNavigate();
+
+  //   const inputTitle = (event) => {
+  //     setTitle(event.target.value);
+  //   };
+
+  //   const inputPrice = (event) => {
+  //     setPrice(event.target.value);
+  //   };
+
+  useEffect(() => {
+    getProductById();
+  }, []);
+
+  const getProductById = async () => {
+    const response = await fetch(
+      `https://6437bd670c58d3b14577f90d.mockapi.io/products/myproducts/${id}`
+    );
+    const data = await response.json();
+    setTitle(data.title);
+    // setPrice(data.price);
+  };
+
+  // const updateTable = async(event) =>{
+  //     event.preventDefault();
+  //     const product = {title, price};
+  //     await fetch(`http://localhost:8080/products/${id}`, {
+  //         method: 'PUT',
+  //         body: JSON.stringify(product),
+  //         headers: {
+  //             'content-type': 'application/json'
+  //         }
+  //     });
+  //     navigate('/table');
+  // }
+  return (
+    <div>
+      {/* <form className='m-4' onSubmit={updateTable}>
+                <label className='block'>Title</label>
+                <input className='border border-slate-400 p-3 h-8' type='text' value={title} onChange={inputTitle}/>
+
+                <label className='block mt-4'>Price</label>
+                <input className='border border-slate-400 p-3 h-8' type='text' value={price} onChange={inputPrice}></input>
+                <button className='bg-cyan-400 p-2 px-4 rounded-lg block mt-4' type='submit'>Update</button>
+            </form> */}
+      <p>{title}</p>
+      {/* <p>{price}</p> */}
+    </div>
+  );
+};
+
+export default EditTable;
